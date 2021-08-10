7d30e63f3d3f62084522e4338317a4caa050b316
pratik mane <pratik.mane1299@gmail.com>
2021-08-10 18:52:55 +0530

    :sparkles: Create a simple tic tac toe game

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function checkWinner(board: string[]) {
+  const possibleCombinations = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+
+  for (let i = 0; i < possibleCombinations.length; i++) {
+    const [idx1, idx2, idx3] = possibleCombinations[i];
+    if (board[idx1] && board[idx1] === board[idx2] && board[idx1] === board[idx3]) {
+      return board[idx1];
+    }
+  }
+  return null;
+}
+
+const initialBoardState = Array(9).fill(null);
+
+function App(props: {}) {
+  const [board, setBoard] = useState<string[]>(initialBoardState);
+
+  const [isXNext, setIsXNext] = useState(true);
+  const [winner, setWinner] = useState<string | null>(null);
+
+  function handleOnBoxClicked(index: number) {
+
+    if (!board[index]) {
+      const v = isXNext ? 'X' : 'O';
+      const newBoardState = board.map((box, i) => (i === index ? v : box));
+
+      setBoard(newBoardState);
+      setIsXNext(!isXNext);
+
+      const winner = checkWinner(newBoardState);
+      if (winner) {
+        setWinner(winner === 'X' ? 'Player X won' : 'Player O won')
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="game-container">
+      <header>
+        <h1>Tic Tac Toe</h1>
       </header>
+      {winner && (
+        <span className="winner-msg">{winner}</span>
+      )}
+      <div className="players">
+        <span className={isXNext ? 'active' : ''}>X's Turn</span>
+        <span className={!isXNext ? 'active' : ''}>O's Turn</span>
+      </div>
+      <div className="board">
+        {board.map((value, i) => (
+          <div onClick={(e) => handleOnBoxClicked(i)} className={`box box-${i}`} key={i}>
+            <span>{value}</span>
+          </div>
+        ))}
+      </div>
+      <button className="reset-btn" onClick={() => {
+        setBoard(initialBoardState);
+        setIsXNext(true);
+        setWinner(null);
+      }}>reset</button>
     </div>
   );
 }
